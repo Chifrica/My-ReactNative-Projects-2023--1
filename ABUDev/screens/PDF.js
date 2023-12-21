d255b5ef78ba14a9fa051145276bc71187fc8395
Blessing Onwunali <chikaonwunali2019@gmail.com>
2023-12-21 03:32:41 +0100

    My Fifth commit

--- ABUDev/screens/PDF.js
+++ ABUDev/screens/PDF.js
@@ -1,16 +1,37 @@
 import * as React from 'react';
 import {View, Text, ScrollView, Pressable, StyleSheet, ImageBackground, Platform} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import MainHeader from '../component/shared/MainHeader';
-import MapView from 'react-native-maps';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import MainHeader from '../component/shared/MainHeader';
+// import MapView from 'react-native-maps';
 
+import DocumentPicker from 'react-native-document-picker';
+import PDFReader from './PDFReader';
 
 
 const PDF = ({navigation}) => {
+    const [pdfUri, setPdfUri] = useState(null);
+    const pickDocument = async () => {
+        try {
+            const res = await DocumentPicker.pick({
+                type: [DocumentPicker.types.pdf],
+            });
+            setPdfUri(res.uri);
+        } catch (err) {
+            if (DocumentPicker.isCancel(err)){
+                console.log('User cancelled the picker');
+            } else {
+                console.log('Error picking the document', err)
+            }
+        }
+    };
     return (
         <View style={styles.container}>
-            <Text>Welcome to PDF Screen</Text>
+            {pdfUri ? (
+                <PDFReader pdfUri={pdfUri} />
+            ) : (
+                <Button title="Select PDF" onPress={pickDocument} />
+            )}
         </View>
     )
 }
---
+++ ABUDev/screens/PDFReader.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View } from 'react-native';
+import Pdf from 'react-native-pdf';
+
+const PDFReader = ({ pdfUri }) => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Pdf
+        source={{ uri: pdfUri, cache: true }}
+        onLoadComplete={(numberOfPages, filePath) => {
+          console.log(`Number of pages: ${numberOfPages}`);
+        }}
+        onPageChanged={(page, numberOfPages) => {
+          console.log(`Current page: ${page}`);
+        }}
+        onError={(error) => {
+          console.log(error);
+        }}
+        style={{ flex: 1 }}
+      />
+    </View>
+  );
+};
+
+export default PDFReader;
